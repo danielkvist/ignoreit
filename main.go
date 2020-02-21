f8a699d3fa152f9342964a0c0d3d74f277939068
Daniel <danielkvist@protonmail.com>
2020-02-21 11:08:40 +0100

    refactor run func

--- main.go
+++ main.go
@@ -9,41 +9,89 @@ import (
 	"strings"
 )
 
+// TODO: Add tests
+// TODO: End README
+
 func main() {
 	if err := run(os.Args, os.Stdout); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
+
+	fmt.Println(".gitignore created successfully!")
 }
 
 func run(args []string, stdout io.Writer) error {
+	filename := ".gitignore"
+
+	if checkIfExists(filename) {
+		return fmt.Errorf("%s", ".gitignore already exists!")
+	}
+
 	if len(args) < 1 {
 		return fmt.Errorf("not enough arguments received")
 	}
 
+	url := createURL("https://gitignore.io/api/", args[1:])
+	data, err := fetch(url)
+	if err != nil {
+		return err
+	}
+
+	file, err := createFile(filename)
+	if err != nil {
+		return fmt.Errorf("while creating %q file: %v", filename, err)
+	}
+	defer file.Close()
+
+	if err := writeTo(file, data); err != nil {
+		return fmt.Errorf("while writing %q content: %v", filename, err)
+	}
+
+	return nil
+}
+
+func checkIfExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
+}
+
+func createURL(url string, params []string) string {
 	// The last comma doesn't produce any error
 	// on the request.
-	list := strings.Join(args[1:], ",")
-	url := "https://gitignore.io/api/" + list
+	return url + strings.Join(params, ",")
+}
+
+func fetch(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return fmt.Errorf("while making request to %q: %v", url, err)
+		return nil, fmt.Errorf("while making request to %q: %v", url, err)
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll((resp.Body))
 	if err != nil {
-		return fmt.Errorf("while reading response body from %q: %v", url, err)
+		return nil, fmt.Errorf("while reading response body from %q: %v", url, err)
 	}
 
-	file, err := os.Create(".gitignore")
+	data := make([]byte, len(body))
+	_ = copy(data, body)
+	return data, nil
+}
+
+func createFile(filename string) (*os.File, error) {
+	file, err := os.Create(filename)
 	if err != nil {
-		return fmt.Errorf("while creating .gitnore file: %v", err)
+		return nil, fmt.Errorf("while creating .gitnore file: %v", err)
 	}
-	defer file.Close()
 
-	if _, err := file.Write(body); err != nil {
-		return fmt.Errorf("while writing .gitignore content: %v", err)
+	return file, nil
+}
+
+func writeTo(w io.Writer, data []byte) error {
+	_, err := w.Write(data)
+	if err != nil {
+		return err
 	}
 
 	return nil
